3c5d25bdedb406e1de1abc2235be36195f3e9c7c
FaizanRasool402 <faizanrasooldeveloper@gmail.com>
2026-03-01 05:59:05 +0500

    Ho gya

---
+++ app/Contactus/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactUsPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-[#182b68]">Contact Us</h1>
+    </div>
+  );
+}
